7523e8e4fdc711fb5acd0ec24883be07706f5284
akratsios <akratsios18@gmail.com>
2016-06-13 01:12:32 -0400

    main and script working

--- script.py
+++ script.py
@@ -53,7 +53,8 @@ import mdl
 from display import *
 from matrix import *
 from draw import *
-
+from os import mkdir
+from shutil import rmtree
 
 """======== first_pass( commands, symbols ) ==========
 
@@ -73,6 +74,37 @@ from draw import *
   jdyrlandweaver
   ==================== """
 def first_pass( commands ):
+
+    frameCheck = varyCheck = nameCheck = 0
+    name = ''
+    num_frames = 1
+
+    for command in commands:
+        
+        if command[0] == 'frames':
+            num_frames = command[1]
+            frameCheck = 1
+
+        elif command[0] == 'vary':
+            varyCheck = 1
+
+        elif command[0] == 'basename':
+            name = command[1]
+            nameCheck = 1
+
+    if varyCheck and not frameCheck:
+        print 'Vary command found without setting number of frames!'
+        exit()
+
+    elif frameCheck and not nameCheck:
+        print 'Animation code present but basename was not set. Using "frame" as basename.'
+        name = 'frame'
+
+    if frameCheck:
+        rmtree('anim', ignore_errors = True)
+        mkdir('anim')
+        
+    return (name, num_frames)
         
 
 """======== second_pass( commands ) ==========
@@ -94,6 +126,33 @@ def first_pass( commands ):
   ===================="""
 def second_pass( commands, num_frames ):
 
+    frames = []
+    for i in range( num_frames ):
+        frames.append( {} )
+        
+    for command in commands:
+        if command[0] == 'vary':
+            knob = command[1]
+            startFrame = command[2]
+            endFrame = command[3]
+            startValue = command[4]
+            endValue = command[5]
+
+            if (startFrame < 0) or (endFrame >= num_frames) or (endFrame < startFrame):
+                print 'Invalid vary command for knob: ' + knob
+                exit()
+            
+            step = (float(endValue) - startValue) / (float(endFrame - startFrame))
+
+            for f in range( startFrame, endFrame+1 ):
+
+                frame = frames[f]
+                
+                value = (f - startFrame) * step + startValue
+
+                frame[knob] = value
+
+    return frames
 
 def run(filename):
     """
@@ -101,6 +160,9 @@ def run(filename):
     """
     color = [255, 255, 255]
     tmp = new_matrix()
+    point_sources = []
+    constants = [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    shading_type = "flat"
     ident( tmp )
 
     p = mdl.parseFile(filename)
@@ -110,95 +172,140 @@ def run(filename):
     else:
         print "Parsing failed."
         return
+
+    (name, num_frames) = first_pass( commands )
+    knobs = second_pass( commands, num_frames )
+    
+    for f in range( num_frames ):
+
+        stack = [ tmp ]
+        screen = new_screen()
+
+        z_buffer = new_matrix(XRES, YRES)
+        for x in xrange(XRES):
+            for y in xrange(YRES):
+                z_buffer[x][y] = -float('inf')
         
-    stack = [ tmp ]
-    screen = new_screen()    
-        
-    for command in commands:
-        if command[0] == "pop":
-            stack.pop()
-            if not stack:
-                stack = [ tmp ]
+        for command in commands:
+            if command[0] == "pop":
+                stack.pop()
+                if not stack:
+                    stack = [ tmp ]
 
-        if command[0] == "push":
-            stack.append( stack[-1][:] )
+            elif command[0] == "push":
+                stack.append( stack[-1][:] )
 
-        if command[0] == "save":
-            save_extension(screen, command[1])
+            elif command[0] == "save":
+                save_extension(screen, command[1])
 
-        if command[0] == "display":
-            display(screen)
+            elif command[0] == "display":
+                display(screen)
 
-        if command[0] == "sphere":
-            m = []
-            add_sphere(m, command[1], command[2], command[3], command[4], 5)
-            matrix_mult(stack[-1], m)
-            draw_polygons( m, screen, color )
+            elif command[0] == "sphere":
+                m = []
+                add_sphere(m, command[1], command[2], command[3], command[4], 5)
+                matrix_mult(stack[-1], m)
+                draw_polygons( m, screen, color, z_buffer, point_sources, constants, shading_type )
 
-        if command[0] == "torus":
-            m = []
-            add_torus(m, command[1], command[2], command[3], command[4], command[5], 5)
-            matrix_mult(stack[-1], m)
-            draw_polygons( m, screen, color )
+            elif command[0] == "torus":
+                m = []
+                add_torus(m, command[1], command[2], command[3], command[4], command[5], 5)
+                matrix_mult(stack[-1], m)
+                draw_polygons( m, screen, color, z_buffer, point_sources, constants, shading_type )
 
-        if command[0] == "box":                
-            m = []
-            add_box(m, *command[1:])
-            matrix_mult(stack[-1], m)
-            draw_polygons( m, screen, color )
+            elif command[0] == "box":                
+                m = []
+                add_box(m, *command[1:])
+                matrix_mult(stack[-1], m)
+                draw_polygons( m, screen, color, z_buffer, point_sources, constants, shading_type )                
 
-        if command[0] == "line":
-            m = []
-            add_edge(m, *command[1:])
-            matrix_mult(stack[-1], m)
-            draw_lines( m, screen, color )
+            elif command[0] == "line":
+                m = []
+                add_edge(m, *command[1:])
+                matrix_mult(stack[-1], m)
+                draw_lines( m, screen, color )
 
-        if command[0] == "bezier":
-            m = []
-            add_curve(m, command[1], command[2], command[3], command[4], command[5], command[6], command[7], command[8], .05, 'bezier')
-            matrix_mult(stack[-1], m)
-            draw_lines( m, screen, color )
+            elif command[0] == "bezier":
+                m = []
+                add_curve(m, command[1], command[2], command[3], command[4], command[5], command[6], command[7], command[8], .05, 'bezier')
+                matrix_mult(stack[-1], m)
+                draw_lines( m, screen, color, z_buffer )
 
-        if command[0] == "hermite":
-            m = []
-            add_curve(m, command[1], command[2], command[3], command[4], command[5], command[6], command[7], command[8], .05, 'hermite')
-            matrix_mult(stack[-1], m)
-            draw_lines( m, screen, color )
+            elif command[0] == "hermite":
+                m = []
+                add_curve(m, command[1], command[2], command[3], command[4], command[5], command[6], command[7], command[8], .05, 'hermite')
+                matrix_mult(stack[-1], m)
+                draw_lines( m, screen, color, z_buffer )
 
-        if command[0] == "circle":
-            m = []
-            add_circle(m, command[1], command[2], command[3], command[4], .05)
-            matrix_mult(stack[-1], m)
-            draw_lines( m, screen, color )
+            elif command[0] == "circle":
+                m = []
+                add_circle(m, command[1], command[2], command[3], command[4], .05)
+                matrix_mult(stack[-1], m)
+                draw_lines( m, screen, color, z_buffer )
 
-        if command[0] == "move":                
-            xval = command[1]
-            yval = command[2]
-            zval = command[3]
+            elif command[0] == "move":
+                xval = command[1]
+                yval = command[2]
+                zval = command[3]
+
+                if command[4]:
+                    knob = knobs[f][command[4]]
+                    xval*= knob
+                    yval*= knob
+                    zval*= knob
                     
-            t = make_translate(xval, yval, zval)
-            matrix_mult( stack[-1], t )
-            stack[-1] = t
+                t = make_translate(xval, yval, zval)
+                matrix_mult( stack[-1], t )
+                stack[-1] = t
 
-        if command[0] == "scale":
-            xval = command[1]
-            yval = command[2]
-            zval = command[3]
+            elif command[0] == "scale":
+                xval = command[1]
+                yval = command[2]
+                zval = command[3]
 
-            t = make_scale(xval, yval, zval)
-            matrix_mult( stack[-1], t )
-            stack[-1] = t
-            
-        if command[0] == "rotate":
-            angle = command[2] * (math.pi / 180)
+                if command[4]:
+                    knob = knobs[f][command[4]]
+                    xval*= knob
+                    yval*= knob
+                    zval*= knob
+                    
+                t = make_scale(xval, yval, zval)
+                matrix_mult( stack[-1], t )
+                stack[-1] = t
 
-            if command[1] == 'x':
-                t = make_rotX( angle )
-            elif command[1] == 'y':
-                t = make_rotY( angle )
-            elif command[1] == 'z':
-                t = make_rotZ( angle )            
+            elif command[0] == "rotate":
+                angle = command[2] * (math.pi / 180)
+
+                if command[3]:
+                    angle*= knobs[f][command[3]]
+
+                if command[1] == 'x':
+                    t = make_rotX( angle )
+                elif command[1] == 'y':
+                    t = make_rotY( angle )
+                elif command[1] == 'z':
+                    t = make_rotZ( angle )            
+                    
+                matrix_mult( stack[-1], t )
+                stack[-1] = t
+
+            elif command[0] == "shading":
+                shading_type = command[1]
                 
-            matrix_mult( stack[-1], t )
-            stack[-1] = t
+            elif command[0] == "ambient":
+                color = command[1:]
+
+            elif command[0] == "light":
+                point_sources.append(list(command[1:]))
+
+            elif command[0] == "constants":
+                constants = command[1:]
+        
+        if num_frames > 1:
+            fname = 'anim/%s%03d.png' % (name, f)
+            print 'Drawing frame: ' + fname
+            save_extension(screen, fname)
+            for x in xrange(XRES):
+                for y in xrange(YRES):
+                    z_buffer[x][y] = -float('inf')
             
